7c98e31ccbb001cc4f5f82338e59b0aa3a513802
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-09 16:08:17 +0300

    refactor: Update loggedin page layout and components for improved user experience

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,44 +1,17 @@
-import Image from "next/image";
-
 import Tasks from "@/components/tasks/Tasks";
-import UserProfile from "@/app/components/UserProfile";
+import Sidebar from "@/components/nav/Sidebar";
+import MobileNav from "@/components/nav/MobileNav";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-6xl font-bold">Welcome to AIssist</h1>
-      <div className="flex flex-row gap-5 py-5">
-        <Image
-          src="/AIssist logo.png"
-          alt="AIssist Logo"
-          // className="dark:invert"
-          width={500}
-          height={500}
-          priority
-        />
-        <Image
-          src="/AIssist logo.svg"
-          alt="AIssist Logo"
-          // className="dark:invert"
-          width={500}
-          height={500}
-          priority
-        />
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <Sidebar />
+      <div className="flex flex-col">
+        <MobileNav />
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
+          <h1 className="text-6xl font-bold">Welcome to AIssist</h1>
+        </main>
       </div>
-      <p className="text-2xl">
-        Welcome to our advanced AI-powered todo application! This innovative app
-        combines traditional task management with cutting-edge artificial
-        intelligence to revolutionize your productivity. Seamlessly add, delete,
-        and organize your tasks while leveraging AI-driven features that
-        intelligently suggest missing items and optimize your to-do list. With
-        robust authentication for secure access and a powerful search
-        functionality, managing your tasks has never been easier or more
-        efficient. Experience the future of task management with our AI-enhanced
-        todo app, designed to streamline your workflow and boost your
-        productivity to new heights.
-      </p>
-      <UserProfile />
-      <Tasks />
-    </main>
+    </div>
   );
 }
