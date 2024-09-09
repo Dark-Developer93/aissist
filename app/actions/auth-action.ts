4bd4153db13902b0b48f4268b080d09c36f0b8fd
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-09 16:05:03 +0300

    refactor(auth-actions): Update sign-out action to use redirectTo parameter

--- app/actions/auth-action.ts
+++ app/actions/auth-action.ts
@@ -1,15 +1,11 @@
 "use server";
 
 import { signIn, signOut } from "@/auth";
-import { redirect } from "next/navigation";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/loggedin" });
-  //   if (result?.error) throw result.error;
-  //   return redirect("/");
 }
 
 export async function signOutAction() {
-  await signOut();
-  return redirect("/");
+  await signOut({ redirectTo: "/" });
 }
