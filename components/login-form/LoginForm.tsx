f553e7924cb0be70018dd217f5f6ed5093cff081
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-09 16:06:53 +0300

    refactor: Update login form component and related files for improved user experience

--- components/login-form/LoginForm.tsx
+++ components/login-form/LoginForm.tsx
@@ -11,8 +11,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 export const description =
   "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account.";
@@ -32,6 +33,27 @@ export function LoginForm() {
       </CardHeader>
       <CardContent>
         <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link href="#" className="ml-auto inline-block text-sm underline">
+                Forgot your password?
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full" disabled>
+            Login
+          </Button>
           <Button
             variant="outline"
             className="w-full"
@@ -39,6 +61,12 @@ export function LoginForm() {
           >
             Login with Google
           </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Does not have an account?{" "}
+          <Link href="/auth/signup" className="underline">
+            Sign up
+          </Link>
         </div>
       </CardContent>
     </Card>
