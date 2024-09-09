e3b198fdc958005994502d66fab6abe6e1865853
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-09 16:06:35 +0300

    refactor: Remove signin page component and related files

--- app/auth/siginin/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/login-form/LoginForm";
-
-export default function LoginPage() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <LoginForm />
-    </div>
-  );
-}
