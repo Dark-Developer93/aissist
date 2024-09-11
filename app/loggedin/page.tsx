119c2dbd523a9a69f722ce2613d2e9df85302caf
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-11 13:35:06 +0300

    refactor: Update loggedin page layout and components for improved user experience

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,6 +1,6 @@
-import Tasks from "@/components/tasks/Tasks";
 import Sidebar from "@/components/nav/Sidebar";
 import MobileNav from "@/components/nav/MobileNav";
+import TodoList from "@/components/todos/TodoList";
 
 export default function Home() {
   return (
@@ -8,8 +8,8 @@ export default function Home() {
       <Sidebar />
       <div className="flex flex-col">
         <MobileNav />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
-          <h1 className="text-6xl font-bold">Welcome to AIssist</h1>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8 xl:px-40">
+          <TodoList />
         </main>
       </div>
     </div>
