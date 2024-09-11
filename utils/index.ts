2157009895e7bb729be2038a1c6d9eda6b1813f7
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-11 17:58:16 +0300

    feat: Add constants for get started project and label IDs

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,4 @@
 export { formatDate } from "./date";
 export { primaryItems } from "./navigationItems";
+export const GET_STARTED_PROJECT_ID = "k17b2fkqe8k2dm3n59nc85rzm970fn69";
+export const GET_STARTED_LABEL_ID = "jx721pm26fd43f6h237f88v3j170eyb1";
