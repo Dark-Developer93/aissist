722c19fcd3241c6b1e89144644ab9ece5f371063
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-11 13:34:53 +0300

    feat: Add formatDate utility function for formatting dates

---
+++ utils/date.ts
@@ -0,0 +1,8 @@
+export const formatDate = (date: string | number | Date) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return new Date(date).toLocaleDateString("en-US", options);
+};
