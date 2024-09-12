dd643d1b6e52254b562a69fc4eb4a8cf3b9fcbe9
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-12 15:04:07 +0300

    refactor(date): Add functions for getting start and end of day timestamps

--- utils/date.ts
+++ utils/date.ts
@@ -6,3 +6,15 @@ export const formatDate = (date: string | number | Date) => {
   };
   return new Date(date).toLocaleDateString("en-US", options);
 };
+
+export const getStartOfDay = (date: Date = new Date()): number => {
+  const start = new Date(date);
+  start.setHours(0, 0, 0, 0);
+  return start.valueOf();
+};
+
+export const getEndOfDay = (date: Date = new Date()): number => {
+  const end = new Date(date);
+  end.setHours(23, 59, 59, 999);
+  return end.valueOf();
+};
