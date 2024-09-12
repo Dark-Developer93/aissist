ef1592006af3724d584497d2e11071cb064f26d8
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2024-09-12 16:21:44 +0300

    refactor(add-tasks): Add SuggestMissingTasks component to AddTaskDialog

---
+++ components/add-tasks/SuggestMissingTasks.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Loader2 } from "lucide-react";
+import { useAction } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Button } from "../ui/button";
+
+export default function SuggestMissingTasks({
+  projectId,
+  isSubTask = false,
+  taskName = "",
+  description = "",
+  parentId,
+}: {
+  projectId: Id<"projects">;
+  isSubTask?: boolean;
+  taskName?: string;
+  description?: string;
+  parentId?: Id<"todos">;
+}) {
+  const [isLoadingSuggestMissingTasks, setIsLoadingSuggestMissingTasks] =
+    useState(false);
+
+  const suggestMissingTasks =
+    useAction(api.queries.ai.suggestMissingItemsWithAi) || [];
+
+  const suggestMissingSubTasks =
+    useAction(api.queries.ai.suggestMissingSubItemsWithAi) || [];
+
+  const handleMissingTasks = async () => {
+    setIsLoadingSuggestMissingTasks(true);
+    try {
+      await suggestMissingTasks({ projectId });
+    } catch (error) {
+      console.log("Error in suggestMissingTasks", error);
+    } finally {
+      setIsLoadingSuggestMissingTasks(false);
+    }
+  };
+
+  const handleMissingSubTasks = async () => {
+    setIsLoadingSuggestMissingTasks(true);
+    try {
+      if (parentId) {
+        await suggestMissingSubTasks({
+          projectId,
+          taskName,
+          description,
+          parentId,
+        });
+      }
+    } catch (error) {
+      console.log("Error in suggestMissingSubTasks", error);
+    } finally {
+      setIsLoadingSuggestMissingTasks(false);
+    }
+  };
+
+  return (
+    <>
+      <Button
+        variant={"outline"}
+        disabled={isLoadingSuggestMissingTasks}
+        onClick={isSubTask ? handleMissingSubTasks : handleMissingTasks}
+      >
+        {isLoadingSuggestMissingTasks ? (
+          <div className="flex gap-2">
+            Loading Tasks (AI)
+            <Loader2 className="h-5 w-5 text-primary animate-spin" />
+          </div>
+        ) : (
+          "Suggest Missing Tasks (AI) 💖"
+        )}
+      </Button>
+    </>
+  );
+}
